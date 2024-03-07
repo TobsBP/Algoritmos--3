8d0693252114e88942d6f5ab1abec773e4821f2c
TobsBP <tobias.bueno2005@gmail.com>
2024-03-07 16:27:37 -0300

    Ilha resolvido

--- Divisão_Conquista/Ilha.cpp
+++ Divisão_Conquista/Ilha.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int buscaBinaria(string arr[], int inicio, int fim, string pista) {
-    if (fim >= inicio) {
-        int meio = inicio + (fim - inicio) / 2;
+int func(string pistas[], int begin, int size, string find)
+{   
+    if (size >= 1)
+    {
+        int meio = begin + (size - 1) / 2;    
 
-        if (arr[meio] == pista)
-            return meio;
+        if (pistas[meio][0] == find[0])
+            return meio;    
 
-        if (arr[meio] > pista)
-            return buscaBinaria(arr, inicio, meio - 1, pista);
-
-        return buscaBinaria(arr, meio + 1, fim, pista);
+        if (find[0] < pistas[meio][0])
+            return func(pistas, meio - 1, begin, find);
+        else
+            return func(pistas, size - meio, meio + 1, find);
     }
+
     return -1;
 }
 
 int main()
 {
+    string pistas[10];
+    string dica;
 
-  return 0;
+    for (int i = 0; i < 7; i++)
+    {
+        getline(cin, pistas[i]);
+        cin.ignore();
+    }
+
+    cin >> dica;
+
+    int res = func(pistas, 0, 7, dica);
+
+    cout << "Índice da pista encontrada: " << res << endl;
+
+    return 0;
 }
